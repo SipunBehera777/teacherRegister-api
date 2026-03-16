1ad236058cf29e53e513f8901552039113d3ef6b
SipunBehera777 <sipun2300@gmail.com>
2026-03-17 00:49:48 +0530

    controller correction

--- model/qrAttendanceModel.js
+++ model/qrAttendanceModel.js
@@ -2,80 +2,76 @@ const db = require("../Config/db");
 
 class QRAttendance {
 
-  static startSession(data, callback) {
+    static startSession(data, callback) {
 
-    const sql = `
-    INSERT INTO attendance_sessions
-    (assignment_id,date,qr_token,start_time,expiry_time,class_latitude,longitude,class_radius)
-    VALUES (?,?,?,?,?,?,?,?)
-    `;
+        const sql = `
+        INSERT INTO attendance_sessions
+        (assignment_id,date,qr_token,start_time,expiry_time,class_latitude,longitude,class_radius)
+        VALUES (?,?,?,?,?,?,?,?)
+        `;
 
-    db.query(sql,
-      [
-        data.assignment_id,
-        data.date,
-        data.qr_token,
-        data.start_time,
-        data.expiry_time,
-        data.class_latitude,
-        data.longitude,
-        data.class_radius
-      ],
-      callback
-    );
-  }
+        db.query(sql, [
+            data.assignment_id,
+            data.date,
+            data.qr_token,
+            data.start_time,
+            data.expiry_time,
+            data.class_latitude,
+            data.longitude,
+            data.class_radius
+        ], callback);
 
+    }
 
-  static getSessionByToken(token, callback) {
+    static checkTodaySession(assignment_id, today, callback) {
 
-    const sql = "SELECT * FROM attendance_sessions WHERE qr_token=?";
+        const sql = `
+        SELECT * FROM attendance_sessions
+        WHERE assignment_id=? AND date=?
+        `;
 
-    db.query(sql, [token], callback);
-  }
+        db.query(sql, [assignment_id, today], callback);
+    }
 
+    static getSessionByToken(token, callback) {
 
-  static markAttendance(data, callback) {
+        db.query(
+            "SELECT * FROM attendance_sessions WHERE qr_token=?",
+            [token],
+            callback
+        );
 
-    const sql = `
-    INSERT INTO attendance_details
-    (attendance_id,status,student_id,marked_time,latitude,longitude)
-    VALUES (?,?,?,?,?,?)
-    `;
+    }
 
-    db.query(sql,
-      [
-        data.attendance_id,
-        data.status,
-        data.student_id,
-        data.marked_time,
-        data.latitude,
-        data.longitude
-      ],
-      callback
-    );
-  }
+    static markAttendance(data, callback) {
 
+        const sql = `
+        INSERT INTO attendance_details
+        (attendance_id,status,student_id,marked_time,latitude,longitude)
+        VALUES (?,?,?,?,?,?)
+        `;
 
-  static checkAlreadyMarked(student_id, attendance_id, callback) {
+        db.query(sql, [
+            data.attendanceid,
+            data.status,
+            data.studentid,
+            data.marked_time,
+            data.latitude,
+            data.longitude
+        ], callback);
 
-    const sql = `
-    SELECT * FROM attendance_details
-    WHERE student_id=? AND attendance_id=?
-    `;
+    }
 
-    db.query(sql, [student_id, attendance_id], callback);
-  }
+    static checkAlreadyMarked(studentid, attendanceid, callback) {
 
-  static checkTodaySession(assignment_id,callback){
+        const sql = `
+        SELECT * FROM attendance_details
+        WHERE student_id=? AND attendance_id=?
+        `;
 
- const sql = `
- SELECT * FROM attendance_sessions
- WHERE assignment_id=? AND DATE(date)=CURDATE()
- `;
+        db.query(sql, [studentid, attendanceid], callback);
 
- db.query(sql,[assignment_id],callback);
-
-}
+    }
 
 }
 
